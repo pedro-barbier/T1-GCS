b6eafa29e73b017f855be23d60f8226104760fd6
barbier1 <162060319+pedro-barbier@users.noreply.github.com>
2026-05-01 22:10:29 -0300

    chore: classe médico adaptada para o padrão do enunciado

--- Sistema de Autorização de Exames Médicos/src/modelo/Medico.java
+++ Sistema de Autorização de Exames Médicos/src/modelo/Medico.java
@@ -1,29 +1,13 @@
 package modelo;
 
 public class Medico extends Usuario {
-    
-    private final String crm;
 
-    public Medico(int identificador, String nome, String crm) {
+    public Medico(int identificador, String nome) {
         super(identificador, nome);
-        
-        if (!ValidadorDeDados.isTextoValido(crm)) {
-            throw new IllegalArgumentException("O CRM do médico é obrigatório e não pode ser vazio.");
-        }
-        
-        if (!ValidadorDeDados.isCrmValido(crm)) {
-            throw new IllegalArgumentException("O CRM informado é muito curto ou inválido.");
-        }
-        
-        this.crm = crm;
     }
 
     @Override
     public String getTipo() {
         return "Médico";
-    }
-    
-    public String getCrm() {
-        return this.crm;
     }
 }
