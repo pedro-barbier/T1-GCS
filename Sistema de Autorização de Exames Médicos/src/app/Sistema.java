3d6f7766bc7776fd06d57fbc965fcee02899ae4a
Henrique Rolim <108377987+henriquerolim01@users.noreply.github.com>
2026-05-11 04:02:46 +0000

    feat: adiciona filtros de autorizações e estatísticas

--- Sistema de Autorização de Exames Médicos/src/app/Sistema.java
+++ Sistema de Autorização de Exames Médicos/src/app/Sistema.java
@@ -63,7 +63,7 @@ public class Sistema {
                     selecionarUsuario();
                     continue;
                 case 2:
-                    if (usuarioAtual.getTipo() == "Administrador"){
+                    if (usuarioAtual.getTipo().equals("Administrador")) {
                         criarNovoUsuario();
                     }
                     break; // aplicar ações específicas para cada tipo de usuário
@@ -71,22 +71,50 @@ public class Sistema {
 
                     if (usuarioAtual instanceof Medico) {
 
-                        Medico medico = (Medico) usuarioAtual;
+                        System.out.println("\nFiltrar por:");
+                        System.out.println("[1] Paciente");
+                        System.out.println("[2] Tipo de exame");
 
-                        System.out.println("\n=== AUTORIZAÇÕES DO MÉDICO ===");
+                        int filtro = Integer.parseInt(scanner.nextLine());
 
-                        for (AutorizacaoExame autorizacao :
-                                autorizacoes.buscarPorMedico(medico)) {
+                        if (filtro == 1) {
 
-                            System.out.println(
-                               "Código: " + autorizacao.getCodigo()
-                                + " | Paciente: " + autorizacao.getPaciente().getNome()
-                                + " | Exame: " + autorizacao.getTipoExame()
-                            );
+                            System.out.print("Nome do paciente: ");
+                            String nomePaciente = scanner.nextLine();
+
+                        Paciente paciente =
+                            (Paciente) usuarios.buscarPorNome(nomePaciente);
+
+                        if (paciente != null) {
+
+                            for (AutorizacaoExame autorizacao :
+                                    autorizacoes.buscarPorPaciente(paciente)) {
+
+                                System.out.println(
+                                    "Código: " + autorizacao.getCodigo()
+                                    + " | Exame: " + autorizacao.getTipoExame()
+                                );
+                            }
                         }
 
-                        // falta implementar filtragem por paciente ou tipo de exame, de acordo com enunciado
+                    } else if (filtro == 2) {
+
+                        System.out.print("Tipo do exame: ");
+                        String tipo = scanner.nextLine();
+
+                        TipoExame tipoExame =
+                            TipoExame.valueOf(tipo.toUpperCase());
+
+                        for (AutorizacaoExame autorizacao :
+                            autorizacoes.buscarPorTipoExame(tipoExame)) {
+
+                            System.out.println(
+                                "Código: " + autorizacao.getCodigo()
+                                + " | Paciente: " + autorizacao.getPaciente().getNome()
+                            );
+                        }
                     }
+                }
                     
                     break; // aplicar ações específicas para cada tipo de usuário
                 case 4:
@@ -115,7 +143,7 @@ public class Sistema {
                     
                     break; // aplicar ações específicas para cada tipo de usuário
                 case 5:
-                    if(usuarioAtual.getTipo() == "Administrador"){
+                    if(usuarioAtual.getTipo().equals("Administrador"))
                         usuarios.listarTodosOsUsuariosCadastradosNoSistema();
                     }
                     break;
